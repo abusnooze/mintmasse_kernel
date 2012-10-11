b50af9a74611bc18afa9f7da73cb6fc12585cd34
Christian Schörkhuber <cschoerkhuber@gmx.at>
2012-10-11 08:51:10 -0700

    that5173.c: code cleaning

--- sound/soc/codecs/that5173.c
+++ sound/soc/codecs/that5173.c
@@ -19,23 +19,17 @@
 #include <sound/initval.h>
 #include <sound/soc.h>
 #include <sound/tlv.h>
-//#include "ad193x.h"
 #include "that5173.h"
 
 /* codec private data */
 struct that5173_priv {
-	//struct regmap *regmap;
-	//int sysclk;
 	struct spi_device *spi;
-	struct snd_soc_codec codec; //(?)
-	int master;
+	struct snd_soc_codec codec;
 };
 
 static const uint8_t that5173_default_regs[] = {
 	0x00, 0x00, 0x00, 0x00,
 };
-
-
 
 
 /* ---------------------------------------------------------------------
@@ -44,15 +38,18 @@ static const uint8_t that5173_default_regs[] = {
 static unsigned int that5173_reg_read(struct snd_soc_codec *codec,
 				   unsigned int reg)
 {
-	struct that5173_priv *that5173 = snd_soc_codec_get_drvdata(codec);
+	//struct that5173_priv *that5173 = snd_soc_codec_get_drvdata(codec);
+	//u8 buffer[8];
+	//int rc;
+
 	u16 *cache = codec->reg_cache;
-	u8 buffer[8];
-	int rc;
 	u16 value;
 
 	printk(KERN_DEBUG "that5173.c->that5173_reg_read: trying to read"); //CS
 
 	value = cache[reg];
+
+	printk(KERN_DEBUG "that5173.c->that5173_reg_read: cached value = %d",value); //CS
 
 	/* this doesn't work: I need to write and read simultaneously. If I use
 	   spi_write_then_read(...) I write 64 bit (cached value) and then I read 64 beats
@@ -60,8 +57,6 @@ static unsigned int that5173_reg_read(struct snd_soc_codec *codec,
 	   I read values different from zero. Hence, for now I just use the cached values and do
 	   not perfrom SPI read at all. This is a bit unsafe as I never actually check the register
 	   content (I would need to use the scope to really see whats in the THAT5173 register.
- 
-	printk(KERN_DEBUG "that5173.c->that5173_reg_read: cached value = %d",value); //CS
 
 	buffer[0] = value; //gain amp 1
 	buffer[1] = 0x30;  //-> update on zerocrossing
@@ -102,7 +97,6 @@ static int that5173_reg_write(struct snd_soc_codec *codec, unsigned int reg,
 
 	struct that5173_priv *that5173 = snd_soc_codec_get_drvdata(codec);
 	u16 *cache = codec->reg_cache;
-	u16 cmd;
 	u8 buffer[8];
 	int rc;
 
@@ -134,80 +128,19 @@ static int that5173_reg_write(struct snd_soc_codec *codec, unsigned int reg,
 
 static const struct snd_kcontrol_new that5173_snd_controls[] = {
 	
-
-	//SOC_DOUBLE_R("Capture Volume", 0x07, 0x08, 0, 20, 0), //maximum value is 20 (= 60 dB gain)
 	SOC_SINGLE("Capture Volume", 0x00, 0, 20, 0), //maximum value is 20 (= 60 dB gain), register address doesn't matter
 
-	/*
-	#define SOC_DOUBLE_R(xname, reg_left, reg_right, xshift, xmax, xinvert) \
-	 {      .iface = SNDRV_CTL_ELEM_IFACE_MIXER, .name = (xname), \
-	        .info = snd_soc_info_volsw, \
-	        .get = snd_soc_get_volsw, .put = snd_soc_put_volsw, \
-	        .private_value = SOC_DOUBLE_R_VALUE(reg_left, reg_right, xshift, \
-	                                            xmax, xinvert) }
-	*/
-
-	/*	
-	SOC_DOUBLE_R("DAC1 Volume", AD193X_DAC_L1_VOL,
-			AD193X_DAC_R1_VOL, 0, 0xFF, 1),
-	SOC_DOUBLE_R("DAC2 Volume", AD193X_DAC_L2_VOL,
-			AD193X_DAC_R2_VOL, 0, 0xFF, 1),
-	SOC_SINGLE_TLV("Left Playback Volume", LM4857_LVOL, 0, 31, 0,
-		stereo_tlv),
-	SOC_SINGLE_TLV("Right Playback Volume", LM4857_RVOL, 0, 31, 0,
-		stereo_tlv),
-	SOC_SINGLE_TLV("Mono Playback Volume", LM4857_MVOL, 0, 31, 0,
-		mono_tlv),
-	*/
-	
 };
 
 static int that5173_probe(struct snd_soc_codec *codec)
 {
-	struct that5173_priv *that5173 = snd_soc_codec_get_drvdata(codec);
-	struct snd_soc_dapm_context *dapm = &codec->dapm;
-	int ret;
-	int tmp; //CS
+	//struct that5173_priv *that5173 = snd_soc_codec_get_drvdata(codec);
+	//struct snd_soc_dapm_context *dapm = &codec->dapm;
 
 	printk(KERN_DEBUG "Entering that5173_probe"); //CS
 
-	//codec->control_data = that5173->regmap;
-	//ret = snd_soc_codec_set_cache_io(codec, 0, 0, SND_SOC_REGMAP);
-	//if (ret < 0) {
-	//	printk(KERN_DEBUG "Failed to set cache I/O: %d\n", ret); //CS
-	//	dev_err(codec->dev, "failed to set cache I/O: %d\n", ret);
-	//	return ret;
-	//}
-
-	//printk(KERN_DEBUG "writing default values to codec registers"); //CS
-	
-	/*Readback register for debugging-------------*/
-	//tmp = snd_soc_read(codec, AD193X_PLL_CLK_CTRL0);
-	/*---------------------------------------------*/
-
-	/*Writing default values to registers---------*/
-	//snd_soc_write(codec, AD193X_PLL_CLK_CTRL0, 0xd8);
-		/*   PLL power-down: normal operation [0 (LSB)]
-		     MCKLI/XI pin functionality (PLL active): INPUT 256 (x44.1 or 48 kHz) [00]
-		   ? MCKLO/XO pin: off (?) [11]
-		     PLL input: ALRCLK [10]
-		     Internal MCLK enable: Enable->ADC active [1] 
-		     ==> 0xd8*/
-	/*--------------------------------------------*/	
-	/*Readback register for debugging-------------*/
-	//tmp = snd_soc_read(codec, AD193X_PLL_CLK_CTRL0);
-	/*---------------------------------------------*/
-	
-	// how should I handle the daisy chain volume thing. Is it possible with controls?
 	snd_soc_add_controls(codec, that5173_snd_controls, ARRAY_SIZE(that5173_snd_controls));
-	/*	
-	snd_soc_dapm_new_controls(dapm, ad193x_dapm_widgets,
-				  ARRAY_SIZE(ad193x_dapm_widgets));
-	snd_soc_dapm_add_routes(dapm, audio_paths, ARRAY_SIZE(audio_paths)); 
-	*/
-	
 
-	//return ret;
 	return 0;
 }
 
@@ -219,25 +152,8 @@ static struct snd_soc_codec_driver soc_codec_dev_that5173 = {
 	.reg_cache_size = ARRAY_SIZE(that5173_default_regs),
 	.reg_word_size = sizeof(uint8_t),
 	.reg_cache_default = that5173_default_regs,
-	
-	/*should I define 
-	.write = lm4857_write,
-	.read = lm4857_read,
-	.probe = lm4857_probe,
-	.reg_cache_size = ARRAY_SIZE(lm4857_default_regs),
-	.reg_word_size = sizeof(uint8_t),
-	.reg_cache_default = lm4857_default_regs,
-	.set_bias_level = lm4857_set_bias_level,
-	?*/
 };
 
-//static const struct regmap_config that5173_spi_regmap_config = {
-//	.val_bits = 8,           //Number of bits in a register value, mandatory.
-//	.reg_bits = 16,          //Number of bits in a register address, mandatory.
-//	.read_flag_mask = 0x09,  //Mask to be set in the top byte of the register when doing a read.
-//	.write_flag_mask = 0x08, //Mask to be set in the top byte of the register when doing a write. 
-//			         //If both read_flag_mask and write_flag_mask are empty the regmap_bus default masks are used.
-//};
 
 static int __devinit that5173_spi_probe(struct spi_device *spi)
 {
@@ -252,36 +168,10 @@ static int __devinit that5173_spi_probe(struct spi_device *spi)
 	that5173 = kzalloc(sizeof(struct that5173_priv), GFP_KERNEL);
 	if (that5173 == NULL)
 		return -ENOMEM;
-	
-	/* Initialize the driver data */
-	//that5173->regmap = regmap_init_spi(spi, &that5173_spi_regmap_config);
-	//if (IS_ERR(that5173->regmap)) {
-	//	ret = PTR_ERR(that5173->regmap);
-	//	goto err_free;
-	//}
-
-	//spi_set_drvdata(spi, that5173);
-
-	//printk(KERN_DEBUG "that5173.c->that5173_spi_probe: calling snd_soc_register_codec"); //CS
-	////ret = snd_soc_register_codec(&spi->dev, &soc_codec_dev_ad193x, &ad193x_dai, 1);
-	//ret = snd_soc_register_codec(&spi->dev, &soc_codec_dev_that5173, NULL, 0); //NULL, 0 as in lm4857.c
-	//if (ret < 0)
-	//	goto err_regmap_exit;
-
-	//printk(KERN_DEBUG "that5173.c->that5173_spi_probe: successful!"); //CS
-	//return 0;
-
-//err_regmap_exit:
-//	regmap_exit(that5173->regmap);
-//err_free:
-//	kfree(that5173);
-
-//	printk(KERN_DEBUG "that5173.c->that5173_spi_probe: return with error"); //CS
-//	return ret;
 
 	that5173->spi = spi;
 	dev_set_drvdata(&spi->dev, that5173);
-	that5173->master = 1; //? don't need that, do I?
+	//that5173->master = 1; //? don't need that, do I?
 
 	ret = snd_soc_register_codec(&spi->dev, &soc_codec_dev_that5173, NULL, 0);
 	if (ret < 0){
@@ -303,11 +193,9 @@ static int __devexit that5173_spi_remove(struct spi_device *spi)
 	printk(KERN_DEBUG "Entering: that5173_spi_remove"); //CS
 
 	snd_soc_unregister_codec(&spi->dev);
-	//regmap_exit(that5173->regmap);
-
-	//spi_unregister_driver(&that5173_spi_driver);
 
 	kfree(that5173);
+
 	return 0;
 }
 
@@ -319,8 +207,6 @@ static struct spi_driver that5173_spi_driver = {
 	.probe		= that5173_spi_probe,
 	.remove		= __devexit_p(that5173_spi_remove),
 };
-//#endif
-
 
 static int __init that5173_modinit(void)
 {
